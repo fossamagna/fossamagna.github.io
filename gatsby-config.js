f4317eb1f98b964d35947af2e470a63c7786a23e
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2022-09-20 18:22:21 +0900

    add blog post for introduce SlackApp in amplify-category-console-notification

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              maxWidth: 1280,
             },
           },
           {
