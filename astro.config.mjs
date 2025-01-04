a4634d6b84ff1214bb07ec23f19e9a2b62b78310
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2025-01-04 23:36:35 +0900

    Add redirects for blog posts in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,26 @@ export default defineConfig({
           shortenUrl: false,
         },
       ],
-      [remarkCodeGithub, {}],
+      remarkCodeGithub,
     ],
+  },
+  redirects: {
+    "/allow-3rd-party-plugin-to-amplify-override":
+      "/blog/allow-3rd-party-plugin-to-amplify-override",
+    "/anatomy-of-amplify-override": "/blog/anatomy-of-amplify-override",
+    "/aws-summit-online-2022": "/blog/aws-summit-online-2022",
+    "/deploy-to-amplify-console": "/blog/deploy-to-amplify-console",
+    "/html5j-webplat-201610": "/blog/html5j-webplat-201610",
+    "/introduce-amplify-backend-vscode":
+      "/blog/introduce-amplify-backend-vscode",
+    "/introduce-amplify-flutter-datastore-extension":
+      "/blog/introduce-amplify-flutter-datastore-extension",
+    "/introduce-slack-app-in-amplify-category-console-notification":
+      "/blog/introduce-slack-app-in-amplify-category-console-notification",
+    "/jawspankration2021": "/blog/jawspankration2021",
+    "/nodefest2016": "/blog/nodefest2016",
+    "/nodefest2017": "/blog/nodefest2017",
+    "/search-by-postal-code-with-amazon-location-service":
+      "/blog/search-by-postal-code-with-amazon-location-service",
   },
 });
