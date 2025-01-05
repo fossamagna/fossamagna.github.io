707d8af0d756ccd64efdb77a9c6767afe3ba6770
MURAKAMI Masahiko <fossamagna2@gmail.com>
2025-01-05 22:52:58 +0900

    Add Partytown integration and Google Tag Manager scripts (#69)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 import remarkLinkCard from "remark-link-card";
 import remarkCodeGithub from "remark-code-github";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fossamagna.github.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     remarkPlugins: [
       [
